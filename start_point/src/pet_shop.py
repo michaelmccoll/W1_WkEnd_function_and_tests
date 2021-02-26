fa18f57eed5379da47e10488c9498a559c316400
michaelmccoll <mccollmichael@hotmail.com>
2021-02-26 19:55:06 +0000

    Initial commit

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,96 @@
+# WRITE YOUR FUNCTIONS HERE
+
+# Test 1 (test_pet_shop_name)  "Camelot of Pets"
+print(" ")
+print("TEST 1")
+
+def get_pet_shop_name()
+
+# Test 2 (test_total_cash)  1000
+# Test 3 (test_add_or_remove_cash__add)  110
+# Test 4 (test_add_or_remove_cash__remove)  990
+print(" ")
+print("TEST 2, 3 ,4")
+
+def get_total_cash()
+
+
+# Test 5 (test_pets_sold)  0
+print(" ")
+print("TEST 5")
+
+def get_pets_sold()
+
+# Test 6 (test_increase_pets_sold)  2
+print(" ")
+print("TEST 6")
+
+def increase_pets_sold()
+
+
+# Test 7 (test_stock_count)  6
+print(" ")
+print("TEST 7")
+
+def get_stock_count()
+
+
+# Test 8 (test_all_pets_by_breed__found)  "British Shorthair" = 2
+# Test 9 (test_all_pets_by_breed__not_found)  "Dalmation" = 0
+print(" ")
+print("TEST 8, 9")
+
+def get_pets_by_breed()
+
+
+# Test 10 (test_find_pet_by_name__returns_pet)  "Arthur"
+# Test 11 (test_find_pet_by_name__returns_None)  "Fred"
+print(" ")
+print("TEST 10, 11")
+
+def find_pet_by_name()
+
+# Test 12 (test_remove_pet_by_name)  "Arthur"
+print(" ")
+print("TEST 12")
+
+def remove_pet_by_name()
+
+# Test 13 (test_add_pet_to_stock)
+print(" ")
+print("TEST 13")
+
+def add_pet_to_stock()
+
+
+# Test 14 (test_customer_cash)
+print(" ")
+print("TEST 14")
+
+def get_customer_cash()
+
+
+# Test 15 (test_remove_customer_cash)
+print(" ")
+print("TEST 15")
+
+def remove_customer_cash()
+
+
+# Test 16 (test_customer_pet_count)
+print(" ")
+print("FUNCTION 10")
+
+def get_customer_pet_count()
+
+
+# Test 17 (test_add_pet_to_customer)
+print(" ")
+print("TEST 17")
+
+def add_pet_to_customer()
+
+
+# 17
+# + 3 OPTIONAL
+# + 3 extra OPTIONAL
